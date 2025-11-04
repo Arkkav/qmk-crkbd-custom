1ae48f7bb51a3c570c995efb63c959bd9a147a61
arkkav <arkark@rambler.ru>
2025-11-04 19:50:51 +0400

    Initial commit: QMK firmware for Corne keyboard with custom keymap

---
+++ keyboards/crkbd/keymaps/my_custom/rules.mk
@@ -0,0 +1,14 @@
+# Basic features
+MACRO_ENABLE = yes
+SEND_STRING_ENABLE = yes
+
+# Unicode configuration
+# UNICODE_ENABLE = yes
+# UNICODE_MODE_LINUX = yes  # закомментировано, т.к. не нужно
+
+# Debug options
+CONSOLE_ENABLE = yes
+DEBUG_MATRIX_SCAN_RATE = yes
+
+# Required for SEND_STRING
+EXTRAKEY_ENABLE = yes
